9c96f640051481b6104494a95cfe34824012fc8e
dhanyn10 <dhanyn.dhan@gmail.com>
2023-12-02 11:44:01 +0700

    make exception to upload route

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/upload' // route location for image upload
+        '/upload-disk', // route location for image upload to disk
+        '/upload-blob'  // route location for image upload to blob data
     ];
 }
