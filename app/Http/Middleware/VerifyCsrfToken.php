fada58e7762661fb308b4006fa9cfffc5cdb7312
dhanyn10 <dhanyn.dhan@gmail.com>
2023-12-02 13:08:22 +0700

    update exception

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/upload-disk', // route location for image upload to disk
-        '/upload-blob'  // route location for image upload to blob data
+        '/upload-disk'  // route location for image upload to blob data
     ];
 }
