f2edbe75230c811ba21b9e37ade23c8e830616ff
dhanyn10 <dhanyn.dhan@gmail.com>
2023-12-02 13:51:51 +0700

    tailwindcdn

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>{{config('app.name')}}</title>
+        <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://cdn.tiny.cloud/1/{{config('tinymce')}}/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-        @vite('resources/css/app.css')
     </head>
     <body>
         @yield('content')
